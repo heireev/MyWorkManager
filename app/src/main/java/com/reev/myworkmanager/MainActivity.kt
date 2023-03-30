f8c337ce8a8af3bae64031c3fc433e46422c35ff
heireev <muhammadrifqi216@gmail.com>
2023-03-30 18:54:21 +0800

    Codelab WorkManager

--- app/src/main/java/com/reev/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/reev/myworkmanager/MainActivity.kt
@@ -2,10 +2,48 @@ package com.reev.myworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.WorkManager
+import com.reev.myworkmanager.databinding.ActivityMainBinding
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnOneTimeTask -> startOneTimeTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+            })
     }
 }
